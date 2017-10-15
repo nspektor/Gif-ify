ff29388852dfbb5b77b443aa768c1e3061e37cc1
Nellie Spektor <nellie.spektor@gmail.com>
2017-10-15 06:27:33 -0400

    something is working

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ from nytimesarticle import articleAPI
 import json
 import urllib,json
 import os
+import requests
 
 app = Flask(__name__)
 
-subject = ""
-api = articleAPI('28bba9543b494e2f9d15976a0fa44f5c')
+
+api = articleAPI('7adeb932f30043dea8bd262313d00d3c')
+urls = []
+keywords = []
 
 @app.route('/')
 def index():
@@ -20,21 +23,51 @@ def category():
 
 @app.route('/category/<name>')
 def hello_name(name):
-    subject = name
-    return 'link: %s!' % getUrls()
+    subject = name.capitalize()
+    print(subject)
+    getUrls(subject)
+    words = []
+    for i in range(len(urls)):
+        print(urls[i] + " " + keywords[i])
+        splitHead = keywords[0].split(' ')
+        word = splitHead[0]
+        words.append(word)
+
+    url = "https://api.giphy.com/v1/gifs/translate"
+
+    s=word[0]
+
+    querystring = {"api_key":"TN5SAJ5tC9a68o8fm1jdqG0hXO2rMNct","s":s}
+
+    response = requests.request("GET", url, params=querystring)
+    jsonR = response.json()
+    data = json.dumps(jsonR)
+    parsed_json = json.loads(data)
+    theURL = parsed_json["data"]["embed_url"]
+    return render_template('category.html', url=theURL)
 
 
-def getUrls():
-    result = []
-    articles = api.search( fq='The New York Times', begin_date='20171010', fl='web_url', sort='newest', facet_field=subject, facet_filter='true')
+def getUrls(subject):
+    articles = api.search( fq='The New York Times', begin_date='20171010', fl='web_url, section_name, headline', facet_field='section_name')
     data = json.dumps(articles)
     parsed_json = json.loads(data)
-    for i in range(0,6):
-        result.append(parsed_json["response"]["docs"][i]["web_url"])
-    return result
+    i = 0
+    while i < 10:
+        result = parsed_json["response"]["docs"][i]
+        #print(result)
+        print(result["section_name"])
+        if(result["section_name"] == subject):
+            urls.append(result['web_url'])
+
+            keywords.append(result['headline']['main'])
+            print("ADDED")
+        i += 1
+    print(urls[0])
+
+
 
 data = json.loads(urllib.urlopen("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=TN5SAJ5tC9a68o8fm1jdqG0hXO2rMNct&limit=5").read())
-print json.dumps(data, sort_keys=True, indent=4)
+#print json.dumps(data, sort_keys=True, indent=4)
 
 
 if __name__ == '__main__':
--- nyt.py
+++ nyt.py
@@ -1,10 +1,18 @@
 from nytimesarticle import articleAPI
 import json
 from pprint import pprint
-api = articleAPI('28bba9543b494e2f9d15976a0fa44f5c')
-articles = api.search( fq='The New York Times', begin_date='20171010', fl='web_url', sort='newest', facet_field='subject=Sports', facet_filter='true')
+api = articleAPI('7adeb932f30043dea8bd262313d00d3c')
+articles = api.search( fq='The New York Times', begin_date='20171010', fl='web_url, section_name', sort='newest', facet_field='section_name', facet_filter='true')
 data = json.dumps(articles)
 parsed_json = json.loads(data)
 pprint(parsed_json)
-for i in range(0,6):
-    print(parsed_json["response"]["docs"][i]["web_url"])
+counter = 0
+for i in range(0,10):
+    if(counter < 6):
+        result = parsed_json["response"]["docs"][i]
+    #print(result["section_name"])
+        if(result["section_name"] == "Sports"):
+            counter+=1
+            print(result["web_url"])
+    else:
+        break
